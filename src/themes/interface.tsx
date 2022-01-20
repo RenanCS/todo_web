ad8f8f529df66b68422a03118d506bf80678f38c
renan <renan@interprocess.com.br>
2022-01-19 21:32:16 -0300

    feat: adiciona interface theme

---
+++ src/themes/interface.tsx
@@ -0,0 +1,16 @@
+
+const fontSizes: any = [14, 20, 96];
+fontSizes.body = fontSizes[0]
+fontSizes.bodyExtraLarge = fontSizes[3]
+fontSizes.displayLarge = fontSizes[2]
+
+const primary: string = '#2567B4'
+const secundary: string = '#F9B531'
+
+export const theme = {
+  fontSizes,
+  colors: {
+    primary,
+    secundary
+  }
+}
